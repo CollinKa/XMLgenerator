83382a262e726c61b19273004c4ab041de0ef41b
Matt Joyce <joyce.279@osu.edu>
2023-08-23 14:44:16 -0400

    change the setting for pixel mahine

--- fakeSetup.sh
+++ fakeSetup.sh
@@ -1,4 +1,7 @@
 export Ph2_ACF_VERSION="v4-14"
-export GUI_dir=$PWD
-export PH2ACF_BASE_DIR=$PWD/Ph2_ACF
+#export GUI_dir=$PWD
+#export PH2ACF_BASE_DIR=$PWD/Ph2_ACF
+#point them toward my personal directory
+export GUI_dir="/home/RD53A/workspace/collin/Ph2_ACF_GUI_Main/Ph2_ACF_GUI"
+export PH2ACF_BASE_DIR="/home/RD53A/workspace/collin/Ph2_ACF_GUI_Main/Ph2_ACF_GUI/Ph2_ACF"
 #python3 XMLUtil.py 

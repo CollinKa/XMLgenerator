8a90542aeba3a0328975f776ff0a514024df8238
Matt Joyce <joyce.279@osu.edu>
2023-08-23 16:38:54 -0400

    update

--- fakeSetup.sh
+++ fakeSetup.sh
@@ -4,4 +4,7 @@ export Ph2_ACF_VERSION="v4-14"
 #point them toward my personal directory
 export GUI_dir="/home/RD53A/workspace/collin/Ph2_ACF_GUI_Main/Ph2_ACF_GUI"
 export PH2ACF_BASE_DIR="/home/RD53A/workspace/collin/Ph2_ACF_GUI_Main/Ph2_ACF_GUI/Ph2_ACF"
-#python3 XMLUtil.py 
+export XMLlocation="/home/RD53A/workspace/collin/PH2_ACF_special/Ph2_ACF/test"
+
+#do the source setup for the Ph2_ACF submodule to enable fpgaconfig & CMSITminiDAQ command
+source /home/RD53A/workspace/collin/PH2_ACF_special/Ph2_ACF/setup.sh
